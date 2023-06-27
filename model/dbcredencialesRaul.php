4ae7e0b3f6f05dc6fc9010b4a6681a3d418a1c80
Tecnologías Web <jorgesltm@gmail.com>
2023-06-27 02:46:08 +0200

    credenciales

--- model/dbcredencialesRaul.php
+++ model/dbcredencialesRaul.php
@@ -1,8 +1,8 @@
 <?php
 
-DEFINE('DB_HOST', 'localhost');
-DEFINE('DB_USER', 'raul');
-DEFINE('DB_PASSWD', 'raul1234');
+DEFINE('DB_HOST', '127.0.0.1');
+DEFINE('DB_USER', 'jorgeslt2223');
+DEFINE('DB_PASSWD', 'pIxtYsHM');
 DEFINE('DB_DATABASE', 'proyectoTW');
 
 ?>
